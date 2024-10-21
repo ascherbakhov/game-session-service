6226067c3b5c7d1c8d6d5d939eec47ceba94da06
ascherbakhov <a.scherbakhov@gmail.com>
2024-10-21 23:48:30 +0200

    Db session creation fixed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base, sessionmaker, Session
-from fastapi import FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, HTTPException, Depends
 
 from GameSessionDAO import GameSessionDAO, get_db
 
@@ -13,19 +13,19 @@ app = FastAPI()
 
 # API endpoints
 @app.post("/sessions/start/")
-def start_session(user_id: str, db: Session = next(get_db())):
+def start_session(user_id: str, db: Session = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = dao.create_session(user_id)
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start}
 
 @app.post("/sessions/end/{session_id}")
-def end_session(session_id: int, db: Session = next(get_db())):
+def end_session(session_id: int, db: Session = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = dao.end_session(session_id)
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start, "session_end": session.session_end}
 
 @app.get("/sessions/{session_id}")
-def get_session(session_id: int, db: Session = next(get_db())):
+def get_session(session_id: int, db: Session = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = dao.get_session(session_id)
     if not session:
