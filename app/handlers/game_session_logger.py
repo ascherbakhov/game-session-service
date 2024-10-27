29d8170cdb49e319040c50719dd78585136ededc
ascherbakhov <a.scherbakhov@gmail.com>
2024-10-27 22:31:38 +0100

    Async Session typehints fixed

--- app/handlers/game_session_logger.py
+++ app/handlers/game_session_logger.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, HTTPException, Depends
 
@@ -17,13 +18,13 @@ async def start_session(request: StartSessionRequest, db: Session = Depends(get_
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start}
 
 @app.post("/sessions/end/{session_id}")
-async def end_session(request: StopSessionRequest, db: Session = Depends(get_db)):
+async def end_session(request: StopSessionRequest, db: AsyncSession = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = await dao.end_session(request.session_id)
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start, "session_end": session.session_end}
 
 @app.get("/sessions/{session_id}")
-async def get_session(session_id: int, db: Session = Depends(get_db)):
+async def get_session(session_id: int, db: AsyncSession = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = await dao.get_session(session_id)
     if not session:
@@ -31,7 +32,7 @@ async def get_session(session_id: int, db: Session = Depends(get_db)):
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start, "session_end": session.session_end}
 
 @app.post("/sessions/heartbeat/{session_id}")
-async def heartbeat(session_id: int, db: Session = Depends(get_db)):
+async def heartbeat(session_id: int, db: AsyncSession = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = await dao.update_heartbeat(session_id)
     return {"session_id": session.id, "user_id": session.user_id, "last_heartbeat": session.last_heartbeat}
