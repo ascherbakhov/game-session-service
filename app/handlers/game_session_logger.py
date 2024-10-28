4f21d13c78b379adb94d1248d133e8644d339e4d
ascherbakhov <a.scherbakhov@gmail.com>
2024-10-28 16:57:00 +0100

    Response model fixed

--- app/handlers/game_session_logger.py
+++ app/handlers/game_session_logger.py
@@ -10,29 +10,29 @@ app = FastAPI()
 
 
 # API endpoints
-@app.post("/sessions/start/")
-async def start_session(request: StartSessionRequest, db: AsyncSession = Depends(get_db)) -> StartSessionResponse:
+@app.post("/sessions/start/", response_model=StartSessionResponse)
+async def start_session(request: StartSessionRequest, db: AsyncSession = Depends(get_db)):
     dao = GameSessionDAO(db)
     game_session = GameSession(user_id=request.user_id, platform=request.platform)
     session = await dao.create_session(game_session)
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start}
 
-@app.post("/sessions/end/{session_id}")
-async def end_session(request: StopSessionRequest, db: AsyncSession = Depends(get_db)) -> EndSessionResponse:
+@app.post("/sessions/end/{session_id}", response_model=EndSessionResponse)
+async def end_session(request: StopSessionRequest, db: AsyncSession = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = await dao.end_session(request.session_id)
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start, "session_end": session.session_end}
 
-@app.get("/sessions/{session_id}")
-async def get_session(session_id: int, db: AsyncSession = Depends(get_db)) -> GetSessionResponse:
+@app.get("/sessions/{session_id}", response_model=GetSessionResponse)
+async def get_session(session_id: int, db: AsyncSession = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = await dao.get_session(session_id)
     if not session:
         raise HTTPException(status_code=404, detail="Session not found")
     return {"session_id": session.id, "user_id": session.user_id, "session_start": session.session_start, "session_end": session.session_end}
 
-@app.post("/sessions/heartbeat/{session_id}")
-async def heartbeat(session_id: int, db: AsyncSession = Depends(get_db)) -> HeartbeatResponse:
+@app.post("/sessions/heartbeat/{session_id}", response_model=HeartbeatResponse)
+async def heartbeat(session_id: int, db: AsyncSession = Depends(get_db)):
     dao = GameSessionDAO(db)
     session = await dao.update_heartbeat(session_id)
     return {"session_id": session.id, "user_id": session.user_id, "last_heartbeat": session.last_heartbeat}
