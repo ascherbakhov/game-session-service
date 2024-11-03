f3c28e711d941fa1ebec649422770779d4a2f5d1
ascherbakhov <a.scherbakhov@gmail.com>
2024-11-03 10:35:17 +0100

    fixed engine and sessionmaker initialization

--- app/database/utils.py
+++ app/database/utils.py
@@ -3,14 +3,20 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
+engine = None
+async_session_maker = None
 
+def init_engine():
+    global engine, async_session_maker
+    if engine is None:
+        DATABASE_URL = os.getenv("DATABASE_URL")
+        engine = create_async_engine(DATABASE_URL, echo=True)
+        async_session_maker = async_sessionmaker(
+            bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
+        )
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    DATABASE_URL = os.getenv("DATABASE_URL")
-
-    engine = create_async_engine(DATABASE_URL, echo=True)
-    async_session_maker = async_sessionmaker(
-        bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
-    )
+    if async_session_maker is None:
+        init_engine()
     async with async_session_maker() as db:
         yield db
