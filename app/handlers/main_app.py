db69e1f6b0d3f765a703fdc3d286ec1e3f1355b1
ascherbakhov <a.scherbakhov@gmail.com>
2024-12-05 21:59:53 +0100

    Metrics updated

--- app/handlers/main_app.py
+++ app/handlers/main_app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.handlers.game_session_logger import v1_router
 from app.handlers.users import users_router
@@ -7,3 +8,4 @@ app = FastAPI()
 
 app.include_router(users_router, prefix='/api/v1')
 app.include_router(v1_router, prefix='/api/v1')
+Instrumentator().instrument(app).expose(app)
--- app/handlers/metrics.py
+++
@@ -1,5 +0,0 @@
-from prometheus_fastapi_instrumentator import Instrumentator
-from app.handlers.main_app import app
-
-
-Instrumentator().instrument(app).expose(app)
