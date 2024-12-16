83c19ba2a9f9ef238c00fbd4a5d955ea520a733c
ascherbakhov <a.scherbakhov@gmail.com>
2024-12-16 21:04:17 +0100

    Celery expired sessions fixed

--- app/expired_sessions_cleaner.py
+++ app/expired_sessions_cleaner.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 
 from app.api.v1.dependencies import get_session_service
 from app.core.config import app_config
-from app.core.database import get_db
+from app.core.database import get_db, init_engine
 from app.core.redis import get_cache
 
 celery_app = Celery(__name__, broker=app_config.redis_url)
@@ -17,11 +17,15 @@ celery_app.conf.beat_schedule = {
     },
 }
 celery_app.conf.timezone = "UTC"
+init_engine(app_config.database_url)
 
 
 @celery_app.task(name="celery_end_expired_sessions")
 def celery_end_expired_sessions():
-    db = get_db()
+    async def get_session():
+        session = await anext(get_db())
+        return session
+    db = asyncio.run(get_session())
     cache = get_cache()
     session_service = get_session_service(db=db, cache=cache)
     asyncio.run(session_service.end_expired_sessions())
