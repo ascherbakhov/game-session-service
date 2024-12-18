d8751286e2edfdc3d20fd30fee56a58fc245ffb5
ascherbakhov <a.scherbakhov@gmail.com>
2024-12-18 18:24:45 +0100

    Handling redis exceptions

--- app/database/dao/redis/game_session_cache_dao.py
+++ app/database/dao/redis/game_session_cache_dao.py
@@ -1,7 +1,10 @@
 import json
 from typing import Optional
 
+from redis import RedisError
+
 from app.api.v1.DTOs.game_session import SessionDTO
+from app.core.logging import session_logger
 
 
 class GameSessionCacheDAO:
@@ -12,28 +15,54 @@ class GameSessionCacheDAO:
         self.redis_cache = redis_cache
 
     async def get_session_from_cache(self, session_id: int) -> Optional[dict]:
-        session_data = await self.redis_cache.get(self.SESSION_KEY.format(session_id))
+        try:
+            session_data = await self.redis_cache.get(self.SESSION_KEY.format(session_id))
+        except RedisError as exc:
+            session_logger.exception(f"Exception {exc} occurred while getting sessions from cache")
+            return None
+
         if session_data:
             return json.loads(session_data)
         return None
 
     async def save_session_to_cache(self, session_id: int, session_data: SessionDTO, ttl: int = 3600):
-        await self.redis_cache.set(self.SESSION_KEY.format(session_id), json.dumps(dict(session_data)), ex=ttl)
+        try:
+            await self.redis_cache.set(self.SESSION_KEY.format(session_id), json.dumps(dict(session_data)), ex=ttl)
+        except RedisError as exc:
+            session_logger.exception(f"Exception {exc} occurred while getting sessions from cache")
 
     async def delete_session_from_cache(self, session_id: int):
-        await self.redis_cache.delete(self.SESSION_KEY.format(session_id))
+        try:
+            await self.redis_cache.delete(self.SESSION_KEY.format(session_id))
+        except RedisError as exc:
+            session_logger.exception(f"Exception {exc} occurred while deleting sessions from cache")
+            return None
 
     async def get_current_session_id_for_user(self, user_id: str) -> Optional[int]:
-        session_id = await self.redis_cache.get(self.USER_SESSION_KEY.format(user_id))
+        try:
+            session_id = await self.redis_cache.get(self.USER_SESSION_KEY.format(user_id))
+        except RedisError as exc:
+            session_logger.exception(f"Exception {exc} occurred while getting current session")
+            return None
+
         if session_id:
             return int(session_id)
         return None
 
     async def set_current_session_for_user(self, user_id: str, session_id: int, ttl: int = 3600):
-        await self.redis_cache.set(self.USER_SESSION_KEY.format(user_id), str(session_id), ex=ttl)
+        try:
+            await self.redis_cache.set(self.USER_SESSION_KEY.format(user_id), str(session_id), ex=ttl)
+        except RedisError as exc:
+            session_logger.exception(f"Exception {exc} occurred while setting session")
+            return None
 
     async def invalidate_user_session_if_exists(self, user_id: str):
-        current_session_id = await self.get_current_session_id_for_user(user_id)
-        if current_session_id:
-            await self.delete_session_from_cache(current_session_id)
-            await self.redis_cache.delete(self.USER_SESSION_KEY.format(user_id))
+        try:
+            current_session_id = await self.get_current_session_id_for_user(user_id)
+
+            if current_session_id:
+                await self.delete_session_from_cache(current_session_id)
+                await self.redis_cache.delete(self.USER_SESSION_KEY.format(user_id))
+        except RedisError as exc:
+            session_logger.exception(f"Exception {exc} occurred while invalidating session")
+            return None
