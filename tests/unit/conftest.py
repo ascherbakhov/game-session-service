9cc6aba87f20efd20eadfc76dff3adbf82bdda55
ascherbakhov <a.scherbakhov@gmail.com>
2024-12-19 08:49:53 +0100

    Setup db logs disable

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -8,12 +8,12 @@ from app.core.config import app_config
 from app.database.tables.models import Base
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def async_engine():
-    return create_async_engine(app_config.database_url, echo=True)
+    return create_async_engine(app_config.database_url)
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def async_session_local(async_engine):
     return sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
@@ -21,13 +21,11 @@ def async_session_local(async_engine):
 async def create_db(async_engine):
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    print("Test database created.")
 
 
 async def drop_db(async_engine):
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
-    print("Test database dropped.")
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -35,7 +33,7 @@ def set_test_database_url():
     app_config.database_url = "sqlite+aiosqlite:///file::memory:?cache=shared"
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(autouse=True)
 def setup_test_db(async_engine, async_session_local):
     loop = asyncio.new_event_loop()
     loop.run_until_complete(create_db(async_engine))
